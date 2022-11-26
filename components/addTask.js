475bccbbc59b499098a145ea361abceeb3ed1fa8
Gonzalo <zalotores@gmail.com>
2022-11-25 21:40:27 -0300

    completado carga desde localstorage

--- components/addTask.js
+++ components/addTask.js
@@ -2,17 +2,13 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento) => {
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-}
-
-const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];       //carga lo que tengo grabado en local o carga lista vacia
-    //console.log(taskList);
+
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector('[data-form-date]');
+
+    const value = input.value;
     const date = calendar.value;
     /*
     https://momentjs.com/
@@ -20,35 +16,38 @@ const createTask = (evento) => {
     */
     const dateFormat = moment(date).format("DD/MM/YYYY");
 
-    const value = input.value;
-
-    const task = document.createElement('li');
-    task.classList.add('card');
     input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
+    calendar.value = '';
 
-    //console.log(value, dateFormat);
+    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];       //carga lo que tengo grabado en local o carga lista vacia
     const taskObj = {
         value,
         dateFormat
     };
-
+    //console.log(taskList);
     taskList.push(taskObj);
 
     //sessionStorage.setItem("tasks", JSON.stringify(taskObj));
     localStorage.setItem("tasks", JSON.stringify(taskList));
 
+    const task = createTask(taskObj);
+    list.appendChild(task);
+}
+
+export const createTask = ({value, dateFormat}) => {
+    const task = document.createElement('li');
+    task.classList.add('card');
+
+    const taskContent = document.createElement('div');
+   
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    // task.innerHTML = content;
 
     const dateElement = document.createElement("span");
     dateElement.innerHTML = dateFormat;
-    //console.log(dateElement);
     task.appendChild(taskContent);
     task.appendChild(dateElement);
     task.appendChild(deleteIcon());
